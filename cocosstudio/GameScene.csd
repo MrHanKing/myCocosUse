9fcd96036b2c2da4012f67a6a47002f9590f6313
hanjun <hanjun@git.com>
2017-11-28 22:16:27 +0800

    update

--- cocosstudio/GameScene.csd
+++ cocosstudio/GameScene.csd
@@ -17,6 +17,17 @@
             <FileData Type="Normal" Path="HelloWorld.png" Plist="" />
             <BlendFunc Src="770" Dst="771" />
           </AbstractNodeData>
+          <AbstractNodeData Name="player" ActionTag="1808047885" Tag="8" RotationSkewX="90.0000" RotationSkewY="90.0004" IconVisible="False" LeftMargin="59.6620" RightMargin="850.3380" TopMargin="300.3374" BottomMargin="299.6626" ctype="SpriteObjectData">
+            <Size X="50.0000" Y="40.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="84.6620" Y="319.6626" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.0882" Y="0.4995" />
+            <PreSize X="0.0479" Y="0.0719" />
+            <FileData Type="Normal" Path="player.png" Plist="" />
+            <BlendFunc Src="1" Dst="771" />
+          </AbstractNodeData>
         </Children>
       </ObjectData>
     </Content>
--- src/GameScene.js
+++ src/GameScene.js
@@ -5,18 +5,42 @@ var GameScene = cc.Scene.extend({
     },
 
     init:function () {
-        this.ui = ccs.load(res.Game_json);
-        this.addChild(this.ui.node);
-        this.bg = ccui.helper.seekWidgetByName(this.ui.node, "bg");
-        this.bg.setTexture(res.HelloWorld_png)
-        this.bg.setAnchorPoint(cc.p(0, 0))
-        this.bg.setPosition(cc.p(0, 0))
-        var bgSize = this.bg.getContentSize()
+        this.ui = ccs.load(res.Game_json).node;
+        this.addChild(this.ui);
+        this.bg = ccui.helper.seekWidgetByName(this.ui, "bg");
+        this.bg.setTexture(res.HelloWorld_png);
+        this.bg.setAnchorPoint(cc.p(0, 0));
+        this.bg.setPosition(cc.p(0, 0));
+        var bgSize = this.bg.getContentSize();
 
         this.bg1 = new cc.Sprite(res.HelloWorld_png);
-        this.bg1.setAnchorPoint(cc.p(0, 0))
-        this.bg1.setPosition(cc.p(bgSize.width, 0))
-        this.addChild(this.bg1)
+        this.bg1.setAnchorPoint(cc.p(0, 0));
+        this.bg1.setPosition(cc.p(bgSize.width, 0));
+        this.ui.addChild(this.bg1, 1);
+
+        this.speed = 1;
+
+        this.player = ccui.helper.seekWidgetByName(this.ui, "player");
+        var playerSize = this.player.getContentSize();
+        this.player.setPosition(cc.p(playerSize.height / 2 + 20, bgSize.height / 2));
+        this.player.setLocalZOrder(2);
+        
+        this.hahaha();
+    },
+
+    hahaha:function() {
+        var listener = cc.EventListener.create({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function (keyCode, event) {
+                cc.log("keyPressed keyCode:", keyCode);
+                cc.log("keyPressed:", event);
+            },
+            onKeyReleased: function (keyCode, event) {
+                cc.log("KeyReleased keyCode:", keyCode);
+                cc.log("KeyReleased:", event);
+            },
+        });
+        cc.eventManager.addListener(listener, this.player);
     },
 
     onEnter:function(params) {
@@ -26,8 +50,8 @@ var GameScene = cc.Scene.extend({
 
     update:function(dt) {
         this._super();
-        this.bg.x -= 1;
-        this.bg1.x -= 1;
+        this.bg.x -= this.speed;
+        this.bg1.x -= this.speed;
 
         if (this.bg.x + this.bg.width <= 0) {
             this.setSprite1AfterSprite2(this.bg, this.bg1);
